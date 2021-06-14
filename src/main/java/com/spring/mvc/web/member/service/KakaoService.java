a6ac4be49034a101993ae699621059b2e15cc564
Claude94 <1claude1994@gmail.com>
2021-06-14 14:38:41 +0900

    [21-06-14] 커밋

--- src/main/java/com/spring/mvc/web/member/service/KakaoService.java
+++ src/main/java/com/spring/mvc/web/member/service/KakaoService.java
@@ -13,6 +13,8 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @Log4j2
@@ -42,6 +44,66 @@ public class KakaoService implements OAuthValue, OAuthService {
 
         //응답 데이터를 스트림을 통해 읽음
         return takeResponseData(conn);
+    }
+
+    @Override
+    public Map<String, Object> getKakaoUserInfo(String accessToken) throws Exception {
+        String reqUrl = "https://kapi.kakao.com/v2/user/me";
+
+        URL url = new URL(reqUrl);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+        conn.setRequestMethod("POST");
+        conn.setDoOutput(true);
+
+        conn.setRequestProperty("Authorization", "Bearer " + accessToken);
+        try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()))) {
+
+            //응답 상태코드 200이면 성공
+            int responseCode = conn.getResponseCode();
+            log.info("응답 코드: " + responseCode);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
+
+            //응답데이터를 입력스트림으로부터 읽어내기
+            String responseData = br.readLine();
+            log.info("response-data: " + responseData);
+
+            //JSON문자열을 Gson라이브러리를 사용하여 자바 객체로 파싱
+            JsonParser parser = new JsonParser();
+
+            //JsonElement는 자바로 변환된 JSON객체
+            JsonElement element = parser.parse(responseData);
+
+            //필요한 데이터를 json프로퍼티 키를 사용해서 추출
+            //카카오 어카운트 정보 추출
+            JsonObject kakaoAccount = element.getAsJsonObject().get("kakao_account").getAsJsonObject();
+            String nickName = kakaoAccount.get("profile").getAsJsonObject().get("nickname").getAsString();
+            String profileImgPath = kakaoAccount.get("profile").getAsJsonObject().get("profile_image_url").getAsString();
+            String email = kakaoAccount.get("email").getAsString();
+
+            log.info(nickName);
+            log.info(profileImgPath);
+            log.info(email);
+
+            Map<String, Object> kakaoInfoMap = new HashMap<>();
+            kakaoInfoMap.put("nickName", nickName);
+            kakaoInfoMap.put("profileImg", profileImgPath);
+            kakaoInfoMap.put("email", email);
+
+            log.info("카카오계정정보" + kakaoAccount);
+            return kakaoInfoMap;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
     private String takeResponseData(HttpURLConnection conn) {
@@ -89,6 +151,33 @@ public class KakaoService implements OAuthValue, OAuthService {
             //응답 상태코드 200이면 성공
             int responseCode = conn.getResponseCode();
             log.info("응답 코드: " + responseCode);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //로그아웃 요청
+    public void logout(String accessToken) throws Exception{
+        String reqUrl = "https://kapi.kakako.com/v1/user/logout";
+
+        URL url = new URL(reqUrl);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+        conn.setRequestMethod("POST");
+        conn.setDoOutput(true);
+
+        conn.setRequestProperty("Authorization", "Bearer " + accessToken);
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
+            //응답 상태코드 200이면 성공
+            int responseCode = conn.getResponseCode();
+            log.info("응답 코드: " + responseCode);
+
+            //응답데이터를 입력스트림으로부터 읽어내기
+            String responseData = br.readLine();
+            log.info("response-data: " + responseData);
 
 
         } catch (Exception e) {
--- src/main/webapp/WEB-INF/views/kakao-test.jsp
+++ src/main/webapp/WEB-INF/views/kakao-test.jsp
@@ -1,3 +1,5 @@
+<%@ page contentType="text/html; charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +9,15 @@
     <title>Document</title>
 </head>
 <body>
+    <c:if test="${loginUser == null}">
     <a href="https://kauth.kakao.com/oauth/authorize?client_id=${appKey}&redirect_uri=http://localhost:8181${redirectUri}&response_type=code"><img src="//mud-kage.kakao.com/14/dn/btqbjxsO6vP/KPiGpdnsubSq3a0PHEGUK1/o.jpg" alt="카카오 로그인"></a>
+</c:if>
+<c:if test="${loginUser != null}">
+    <h1>${loginUser.name}님 카카오 로그인 성공</h1>
+    <img src="${profile_path}" alt="프로필사진">
+    <br>
+    <a href="/kakao/logout">로그아웃</a>
+
+</c:if>
 </body>
 </html>
